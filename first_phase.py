9cfcd471c2ee2b7f77adbe0f783ce2e12609d41e
mosheadd <rost1234567890qgp@gmail.com>
2021-12-26 17:13:04 +0300

    Time add

--- first_phase.py
+++ first_phase.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 import random
+import time
 
 
 all_objects = pygame.sprite.Group()
@@ -9,6 +10,8 @@ TIME = 120  # в секундах
 FPS = 60
 Fg = 0.2  # сила притяжения
 fp_clock = pygame.time.Clock()
+health_appearing_chance = 5
+objects_existing_time = 5
 
 
 def load_image(name):
@@ -24,12 +27,23 @@ class Object(pygame.sprite.Sprite):
         self.y = y
         self.start_speed = speed
         self.current_speed = speed
+        self.on_ground = False
+        self.start_time = 0
+        self.current_time = 0
+        self.erase = False
 
     def update(self):
+        if self.on_ground:
+            self.current_time = time.perf_counter()
+            if self.current_time - self.start_time >= float(objects_existing_time):
+                self.erase = True
         if self.rect.top <= 500:
             self.rect = self.rect.move(0, self.current_speed / FPS)
             self.y += self.current_speed / FPS
             self.current_speed += Fg
+        elif not self.on_ground:
+            self.on_ground = True
+            self.start_time = time.perf_counter()
 
 
 class Health(Object):
@@ -51,8 +65,8 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect().move(x, y)
 
     def update(self, *args):
-        print(args)
         timedelta = fp_clock.tick(FPS) / 1000
+        print(timedelta)
         if args:
             if args[0][pygame.K_w] or args[0][pygame.K_SPACE]:
                 self.jumping = True
@@ -63,8 +77,8 @@ class Player(pygame.sprite.Sprite):
 
 
 def first_phase(screen):
-    health = Health(random.randint(5, 795), -50, 5)
-    player = Player("player.png", 400, 500, 123, 6)
+    objects = [Health(random.randint(5, 795), -50, 5)]
+    player = Player("player.png", 400, 500, 160, 6)
     first_phase_running = True
     quiting_from_game = False
     while first_phase_running:
@@ -86,6 +100,9 @@ def first_phase(screen):
         player_group.draw(screen)
         pygame.display.flip()
         fp_clock.tick(FPS)
+        dice = random.randint(1, 100)
+        if dice <= health_appearing_chance and len(objects) <= 4:
+            objects.append(Health(random.randint(5, 795), -50, 5))
     if quiting_from_game:
         pygame.quit()
         return 0
