7188b6142562a79bd5e73d6de8e6d3cd0eb38ad9
mosheadd <rost1234567890qgp@gmail.com>
2021-12-26 13:11:23 +0300

    Completed jump

--- first_phase.py
+++ first_phase.py
@@ -7,7 +7,7 @@ all_objects = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 TIME = 120  # в секундах
 FPS = 60
-g = 1  # ускорение свободного падения
+g = 0.2  # ускорение свободного падения
 fp_clock = pygame.time.Clock()
 
 
@@ -26,10 +26,11 @@ class Object(pygame.sprite.Sprite):
         self.current_speed = speed
 
     def update(self):
-        if self.y <= 500:
+        if self.rect.top <= 500:
             self.rect = self.rect.move(0, self.current_speed / FPS)
             self.y += self.current_speed / FPS
             self.current_speed += g
+            print(1)
 
 
 class Health(Object):
@@ -51,23 +52,18 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect().move(x, y)
 
     def update(self, *args):
-        if self.jumping:
-            self.rect.move(0, -self.current_jump_speed / FPS)
-            self.current_jump_speed -= g
-            self.y -= self.current_jump_speed
-        if self.y >= 500:
-            self.jumping = False
-            self.current_jump_speed = self.start_jump_speed
         if args:
-            if args[0][pygame.K_SPACE]:
+            if args[0][pygame.K_w] or args[0][pygame.K_SPACE]:
                 self.jumping = True
             if args[0][pygame.K_a]:
-                self.rect.left -= self.start_speed / FPS
+                self.rect.left -= self.start_speed
+            if args[0][pygame.K_d]:
+                self.rect.left += self.start_speed
 
 
 def first_phase(screen):
     health = Health(random.randint(5, 795), -50, 5)
-    player = Player("player.png", 400, 200, 150, 3)
+    player = Player("player.png", 400, 500, 3, 6)
     first_phase_running = True
     quiting_from_game = False
     while first_phase_running:
@@ -77,12 +73,18 @@ def first_phase(screen):
                 quiting_from_game = True
             if pygame.key.get_pressed():
                 player_group.update(pygame.key.get_pressed())
+        if player.jumping:
+            player.rect.top -= player.current_jump_speed
+            player.current_jump_speed -= g
+        if player.rect.top >= 500:
+            player.jumping = False
+            player.current_jump_speed = player.start_jump_speed
         screen.fill((255, 255, 255))
         all_objects.draw(screen)
         all_objects.update()
         player_group.draw(screen)
-        fp_clock.tick(FPS)
         pygame.display.flip()
+        fp_clock.tick(FPS)
     if quiting_from_game:
         pygame.quit()
         return 0
