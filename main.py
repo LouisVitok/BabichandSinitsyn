77fd0d34543bafc8d0287e1d337a7e42b0d1fc53
mosheadd <rost1234567890qgp@gmail.com>
2022-01-07 13:53:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -38,12 +38,14 @@ def load_image(name):
 
 def start_screen(width, height):
     intro = ['Выберите сложность', 'Легко', 'Нормально', 'Сложно']
+    rectangles = []
     background = pygame.transform.scale(load_image('zastavka.jpg'), (width, height))
     screen.blit(background, (0, 0))
     y = 5
     for line in intro:
         s = pygame.font.Font(None, 70).render(line, True, (250, 250, 210))
         rect = s.get_rect()
+        rectangles.append(rect)
         rect.x = width // 2 - rect.w // 2
         screen.blit(s, (rect.x, y))
         y += height // 4
@@ -56,6 +58,17 @@ def start_screen(width, height):
                 return
             if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                 return
+        screen.blit(background, (0, 0))
+        for i in range(len(intro)):
+            if rectangles[i].collidepoint(pygame.mouse.get_pos()) and i != 0:
+                color = (255, 10, 10)
+            else:
+                color = (250, 250, 210)
+            s = pygame.font.Font(None, 70).render(intro[i], True, color)
+            rect = s.get_rect()
+            rect.x = width // 2 - rect.w // 2
+            screen.blit(s, (rect.x, y))
+            y += height // 4
         pygame.display.flip()
         clock.tick(FPS)
 
